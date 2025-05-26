551c3c462dd89fa6ad85a61065c8c9453e086a28
Oscar Chocce <oscarchocce08@gmail.com>
2025-05-25 21:16:14 -0500

    prueba configuración

--- src/main/java/Config/clsConnection.java
+++ src/main/java/Config/clsConnection.java
@@ -1,6 +1,5 @@
 
 package Config;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,7 +8,6 @@ public class clsConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/educontrol_db";
     private static final String USER = "root";
     private static final String PASS = ""; // cambia si pusiste contraseña
-
     static {
         try {
             // Registrar el driver explícitamente
@@ -18,7 +16,6 @@ public class clsConnection {
             throw new RuntimeException("Error al cargar el driver JDBC de MySQL", e);
         }
     }
-
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
